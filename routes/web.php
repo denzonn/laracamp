abe8fd4f520da482a6e04e60c82b15db1a2f9cc4
Denzonn <denson753@gmail.com>
2022-10-04 16:12:12 +0800

    feat: Integration Landing, Checkout and Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,21 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout', function () {
-    return view('pages.checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('pages.success_checkout');
-})->name('success-checkout');
-
 // Socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
-
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {  
+    // User Dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+
+    // Checkout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+});
 
 require __DIR__.'/auth.php';
